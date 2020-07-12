5a09c0b0521f47c3d1270c08b405c24c6e8b6e76
mihsana <mihsana@user.github.com>
2020-07-12 13:56:42 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 docker-compose build
 docker-compose push
-scp docker-compose.yaml jenkins@:/home/muhammad_786_ihsan/DevOps-Practical-Project/docker-compose.yaml
+scp docker-compose.yaml jenkins@practical-project:/home/jenkins/docker-compose.yaml
 ssh jenkins@practical-project	 << EOF
-cd home/muhammad_786_ihsan/DevOps-Practical-Project
 env SECRET_KEY=${SECRET_KEY} env DATABASEB_URI=${DATABASE_URI} env DB_PASSWORD=${DB_PASSWORD} docker stack deploy --compose-file docker-compose.yaml
 EOF
