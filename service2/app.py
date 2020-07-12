ed66f92ab7d0da4f064e7d770b78aed69d657fc8
MihsanA <MihsanA@user.github.com>
2020-07-12 22:57:10 +0000

    check

--- service2/app.py
+++ service2/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, Response, Requests
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, Response, 
 import random
 import requests
+
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+
 @app.route('/number', methods=['GET'])
 def number():
-    #person= Person.query.all()
     number = random.randint(1,10)
-    return Response(str(number), mimetype='text/plain')
+    return Response(number, mimetype='text/plain')
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
