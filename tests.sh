c16efaf77585341393347630e819d8e6311b03f5
mihsana <mihsana@user.github.com>
2020-07-12 12:38:45 +0000

    xxx

--- tests.sh
+++ tests.sh
@@ -3,19 +3,19 @@
 run apt update
 run apt install python3-pip -y
 
-run pip3 install -r requirement.txt
+run pip3 install -r service1/requirements.txt
 
-. /home/muhammad_786_ihsan/DevOps-Practical-Project/venv/bin/activate
+#. /home/muhammad_786_ihsan/DevOps-Practical-Project/venv/bin/activate
 
-cd /home/jenkins/.jenkins/workspace/DevOps-Practical-Project/service1
+cd service1
 pytest --cov ./application
 
-cd /home/jenkins/.jenkins/workspace/DevOps-Practical-Project/service2
+cd ../service2
 pytest 
 
-cd /home/jenkins/.jenkins/workspace/DevOps-Practical-Project/service3
+cd ../service3
 pytest 
 
-cd /home/jenkins/.jenkins/workspace/DevOps-Practical-Project/service4
+cd ../service4
 pytest 
 
